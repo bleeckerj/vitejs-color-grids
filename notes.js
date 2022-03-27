a6ff43f7986036617c63a0337e754a7c1f6c9bd7
Julian Bleecker <bleeckerj@gmail.com>
2022-03-26 21:27:26 -0700

    Some pocket notes

--- notes.js
+++ notes.js
@@ -25,3 +25,48 @@ console.log("Left? : " + x + " ; Top? : " + y + ".");
 console.log("Height "+rect.height);
 var qH = y / rect.height;
 console.log("qH="+qH);
+
+
+QmaQxuWGM47vBzgUHGHJMsEs7YrMx7yHCveYfK6Z8MFAP1
+https://cloudflare-ipfs.com/ipfs/QmaQxuWGM47vBzgUHGHJMsEs7YrMx7yHCveYfK6Z8MFAP1
+
+https://cloudflare-ipfs.com/ipfs/QmSpuQaZPFYPLhKssiWYimmep1gxc1zTikNF2AGHj3rnx3
+
+
+https://cloudflare-ipfs.com/ipfs/QmdET7HmcbXnVeFQmEP4YHeH1bsCbq87zJAHRWWestZgMs
+
+
+https://cloudflare-ipfs.com/ipfs/QmReqAJSggAHG2dAmyzXdSLcf1ShLuvg7iWrEibiyhZCBz
+
+https://cloudflare-ipfs.com/ipfs/QmYCEKGA2stWkDT3BsYMaANqUvD3dYJKq7JmRwUcNH5wkc (domtoJpeg with string split and crap)
+
+QmR57wLcWmS4FjsFM5csd2rEhgSVxmDGpUW6ZRyAUwkoqi
+
+https://cloudflare-ipfs.com/ipfs/QmajyYTK6NYduXnKgtc72vDXUZvWM3uvxHJSoZSHbaQ1gx (with json meta data?)
+points to https://cloudflare-ipfs.com/ipfs/QmdQnn7gRac3TMC95UpZLytkBXtCTAyMBQVryFgYPfzshf
+
+
+https://cloudflare-ipfs.com/ipfs/QmQi4tLWJVnU8YEyRzsa5F5yARBG6NRuiPsG3B1PkbJ1EQ
+
+
+https://cloudflare-ipfs.com/ipfs/QmdHpgG67WYm9E9ybV8fvek8q2D6RRjti3oEwdG3yR93AR
+
+https://cloudflare-ipfs.com/ipfs/QmcrQVGPwmN9zLGWznhonXVUXwJNeAKFBjwWZQbhVpb4kJ
+
+QmYnGLX2ppx1s7XT3pscLcVRTNFKSvZESWNsqYaFjMiSNG
+
+Qme8iTMAFvUoWmwaz16Dk4aLJq5ZccQGf13Nz6VNYzAeJ5
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div style="position: relative; top: 0px; left: -35px; width: 800px; height: 800px;" id="blocks">            <div id="block_0" style="position: absolute; top: 0px; left: 0px; box-sizing: content-box; height: 600px; width: 400px; outline: black solid 1px; outline-offset: -1px; background-color: rgb(250, 250, 110); touch-action: manipulation; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></div><div id="block_2" style="position: absolute; left: 0px; top: 650px; height: 150px; width: 400px; outline: black solid 1px; outline-offset: -1px; background-color: rgb(127, 222, 105); touch-action: manipulation; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></div><div id="block_3" style="position: absolute; left: 0px; top: 600px; height: 50px; width: 400px; outline: black solid 1px; outline-offset: -1px; background-color: rgb(65, 208, 103); touch-action: manipulation; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></div><div id="block_1" style="position: absolute; left: 400px; top: 0px; height: 800px; width: 400px; outline: black solid 1px; outline-offset: -1px; background-color: rgb(188, 236, 108); touch-action: manipulation; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></div></div>
